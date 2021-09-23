5bd32e6c4865e97a74428137ee000ac461231964
kosarev-sa <kosarev-sa@mail.ru>
2021-09-24 00:20:58 +0300

    + tasks_lesson4

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -125,5 +126,8 @@ STATICFILES_DIRS = (BASE_DIR / 'static',)
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL = 'users.User'
